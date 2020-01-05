259220796beb0ca78c0db1c44dbd154f5bdf8046
Matt Huszagh <huszaghmatt@gmail.com>
2020-01-04 16:22:58 -0800

    add nix shell file

---
+++ shell.nix
@@ -0,0 +1,35 @@
+{ nixpkgs ? (import <nixpkgs> {})
+}:
+
+let
+  custompkgs = import <custompkgs> {};
+  pkgs = (nixpkgs // custompkgs);
+  libcircuit = pkgs.libcircuit;
+  mh-python = pkgs.python3.withPackages (ps: with ps; [
+    numpy
+    matplotlib
+    scipy
+  ] ++ (with custompkgs; [
+    libcircuit
+    skidl
+  ]));
+  kicad = pkgs.kicad;
+in
+pkgs.mkShell rec {
+  buildInputs = with pkgs; [
+    python3Full
+    mh-python
+    python-openems
+    python-csxcad
+
+    # pcb cad
+    kicad
+
+    # ems
+    (openems.override {withMPI = false; })
+    appcsxcad
+    hyp2mat
+  ];
+
+  KICAD_SYMBOL_DIR="${kicad.out}/share/kicad/library";
+}
