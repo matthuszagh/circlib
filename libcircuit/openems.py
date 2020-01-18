48b926930a3fe8f422e4ff19a85215f71d07b9f8
Matt Huszagh <huszaghmatt@gmail.com>
2020-01-18 13:32:13 -0800

    openems: wheeler formulas for microstrip width/impedance

--- libcircuit/openems.py
+++ libcircuit/openems.py
@@ -546,6 +546,117 @@ common_pcbs = {
 }
 
 
+def wheeler_z0(w: float, t: float, er: float, h: float) -> float:
+    """
+    Calculate the microstrip characteristic impedance for a given width
+    using Wheeler's equation. Wheeler's equation can be found at:
+
+    https://en.wikipedia.org/wiki/Microstrip#Characteristic_impedance
+
+    Args:
+        w: microstrip trace width (mm)
+        t: trace thickness (mm)
+        er: substrate relative permittivity
+        h: substrate height (thickness) (mm)
+
+    Returns:
+        characteristic impedance
+    """
+    z0 = 376.730313668
+    w *= 1e-3
+    t *= 1e-3
+    h *= 1e-3
+    weff = w + (
+        (t * ((1 + (1 / er)) / (2 * np.pi)))
+        * np.log(
+            (4 * np.e)
+            / (
+                np.sqrt(
+                    ((t / h) ** 2)
+                    + ((1 / np.pi) * ((1 / ((w / t) + (11 / 10))) ** 2))
+                )
+            )
+        )
+    )
+    tmp1 = 4 * h / weff
+    tmp2 = (14 + (8 / er)) / 11
+    zm = (z0 / (2 * np.pi * np.sqrt(2 * (1 + er)))) * np.log(
+        1
+        + (
+            tmp1
+            * (
+                (tmp2 * tmp1)
+                + (
+                    np.sqrt(
+                        (
+                            (tmp2 * tmp1) ** 2
+                            + ((np.pi ** 2) * ((1 + (1 / er)) / 2))
+                        )
+                    )
+                )
+            )
+        )
+    )
+    return zm
+
+
+def wheeler_z0_width(
+    z0: float,
+    t: float,
+    er: float,
+    h: float,
+    tol: float = 0.01,
+    guess: float = 0.3,
+):
+    """
+    Calculate the microstrip width for a given characteristic
+    impedance using Wheeler's formula.
+
+    Args:
+        z0: characteristic impedance (ohm)
+        t: trace thickness (mm)
+        er: substrate relative permittivity
+        h: substrate height (thickness) (mm)
+        tol: acceptable impedance tolerance (ohm)
+        guess: an initial guess for the width (mm). This can improve
+               convergence time when the approximate width is known.
+
+    Returns:
+        trace width (mm)
+    """
+    width = guess
+    zm = wheeler_z0(w=width, t=t, er=er, h=h)
+    print(zm)
+    wlow = width / 10
+    zlow = wheeler_z0(w=wlow, t=t, er=er, h=h)
+    # inverse relation between width and z0
+    while zlow < z0:
+        wlow /= 10
+        zlow = wheeler_z0(w=wlow, t=t, er=er, h=h)
+
+    whigh = width * 10
+    zhigh = wheeler_z0(w=whigh, t=t, er=er, h=h)
+    while zhigh > z0:
+        whigh *= 10
+        zhigh = wheeler_z0(w=whigh, t=t, er=er, h=h)
+
+    while np.absolute(zm - z0) > tol:
+        if zm > z0:
+            m = (zhigh - zm) / (whigh - width)
+            wlow = width
+            zlow = zm
+        else:
+            m = (zm - zlow) / (width - wlow)
+            whigh = width
+            zhigh = zm
+
+        # use linear interpolation to update guess
+        width = width + ((z0 - zm) / m)
+        zm = wheeler_z0(w=width, t=t, er=er, h=h)
+
+    return width
+
+
 def microstrip_width_sweep(
     f0: float,
     fc: float,
