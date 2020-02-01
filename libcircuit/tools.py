7a4499d04ed5dbeb6dd0300becd081a7a12c45ad
Matt Huszagh <huszaghmatt@gmail.com>
2020-01-31 19:59:13 -0800

    fix noise figure calculation

--- libcircuit/tools.py
+++ libcircuit/tools.py
@@ -7,19 +7,18 @@ def noise_figure_equiv(inputs: List[List[float]]) -> float:
     Calculate the equivalent noise figure for multiple devices chained
     together.
 
-    :param inputs: A list of lists, where the outer dimensions
-        corresponds to each device in the chain and the inner
-        dimension has two elements, where the first element is the
-        noise figure for that device and the second element is the
-        gain. Both of these values must be given in dB.
+    :param inputs: A 2D list, where the outer dimensions corresponds
+        to each device in the chain and the inner dimension has two
+        elements, where the first element is the noise figure for that
+        device and the second element is the gain.  Both of these
+        values must be given in dB.
 
     :returns: The total equivalent noise figure for all devices (in
               dB).
     """
     n = len(inputs)
-    nf = [elem[0] for elem in inputs]
-    g = [elem[1] for elem in inputs]
-    f = [10 ** (i / 10) for i in nf]
+    f = [10 ** (elem[0] / 10) for elem in inputs]
+    g = [10 ** (elem[1] / 10) for elem in inputs]
     fnet = f[0]
     gnet = g[0]
     for i in range(1, n):
